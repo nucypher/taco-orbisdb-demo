a306b63a9de1adc3cd19adb839042d7bfae482b7
Manuel Montenegro <manuel@nucypher.com>
2024-10-09 18:23:07 +0200

    Modify useTaco hook to use context variables

--- app/hooks/useTaco.ts
+++ app/hooks/useTaco.ts
@@ -1,7 +1,7 @@
+import { useEffect, useContext } from "react";
 import {
   conditions,
   decrypt,
-  domains,
   encrypt,
   ThresholdMessageKit,
 } from "@nucypher/taco";
@@ -10,100 +10,66 @@ import {
   USER_ADDRESS_PARAM_DEFAULT,
 } from "@nucypher/taco-auth";
 import { ethers } from "ethers";
+import { TACoContext } from "../context/TACoContext";
 
-let authManager = {
-  provider: null,
-  authProvider: null,
-  currentAccount: null,
+export default function useTaco() {
+  const { isInitialized, ritualId, domain } = useContext(TACoContext);
 
-  async initialize() {
-    if (!this.provider) {
-      this.provider = new ethers.providers.Web3Provider(window.ethereum);
-      await this.updateAccount();
-      this.setupAccountChangeListener();
-    }
-    return this.authProvider;
-  },
+  async function encryptWithTACo(
+    messageToEncrypt: string,
+    condition: conditions.condition.Condition,
+    provider: ethers.providers.Provider,
+    signer: ethers.Signer,
+  ) {
+    if (!isInitialized) return;
 
-  async updateAccount() {
-    const accounts = await this.provider.listAccounts();
-    this.currentAccount = accounts[0];
-    this.authProvider = new EIP4361AuthProvider(
-      this.provider,
-      this.provider.getSigner(),
-    );
-  },
-
-  setupAccountChangeListener() {
-    window.ethereum.on("accountsChanged", async (accounts) => {
-      const newAccount = accounts[0];
-      if (newAccount !== this.currentAccount) {
-        await this.updateAccount();
-      }
-    });
-  },
-};
-
-export async function getAuthProvider() {
-  return authManager.initialize();
-}
-const rpcCondition = new conditions.base.rpc.RpcCondition({
-  chain: 80002,
-  method: "eth_getBalance",
-  parameters: [":userAddress"],
-  returnValueTest: {
-    comparator: ">",
-    value: 0,
-  },
-});
-
-export async function encryptWithTACo(
-  messageToEncrypt: string,
-): Promise<String> {
-  const provider = new ethers.providers.Web3Provider(window.ethereum);
-  const tmk = await encrypt(
-    provider,
-    domains.TESTNET,
-    messageToEncrypt,
-    rpcCondition,
-    0,
-    provider.getSigner(),
-  );
-  return encodeB64(tmk.toBytes());
-}
-
-export async function decryptWithTACo(
-  encryptedMessage: string,
-): Promise<String> {
-  try {
-    const tmk = ThresholdMessageKit.fromBytes(decodeB64(encryptedMessage));
-    const conditionContext =
-      conditions.context.ConditionContext.fromMessageKit(tmk);
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-
-    const authProvider = await getAuthProvider();
-    console.log(authProvider);
-
-    conditionContext.addAuthProvider(USER_ADDRESS_PARAM_DEFAULT, authProvider);
-
-    const decrypted = await decrypt(
+    const tmk = await encrypt(
       provider,
-      domains.TESTNET,
-      tmk,
-      conditionContext,
+      domain,
+      messageToEncrypt,
+      condition,
+      ritualId,
+      signer,
     );
 
-    return new TextDecoder().decode(decrypted);
-  } catch (error) {
-    console.error("Decryption failed:", error);
-    return encryptedMessage;
+    return encodeB64(tmk.toBytes());
   }
-}
 
-export function encodeB64(uint8Array: any) {
-  return Buffer.from(uint8Array).toString("base64") as String;
-}
+  async function decryptWithTACo(
+    encryptedMessage: string,
+    provider: ethers.providers.Provider,
+    signer: ethers.Signer,
+  ) {
+    if (!isInitialized) return;
 
-export function decodeB64(b64String: any) {
-  return new Uint8Array(Buffer.from(b64String, "base64"));
+    try {
+      const tmk = ThresholdMessageKit.fromBytes(decodeB64(encryptedMessage));
+      const conditionContext =
+        conditions.context.ConditionContext.fromMessageKit(tmk);
+
+      const authProvider = new EIP4361AuthProvider(provider, signer);
+
+      conditionContext.addAuthProvider(
+        USER_ADDRESS_PARAM_DEFAULT,
+        authProvider,
+      );
+
+      const decrypted = await decrypt(provider, domain, tmk, conditionContext);
+
+      return new TextDecoder().decode(decrypted);
+    } catch (error) {
+      console.error("Decryption failed:", error);
+      return encryptedMessage;
+    }
+  }
+
+  function encodeB64(uint8Array: any) {
+    return Buffer.from(uint8Array).toString("base64") as String;
+  }
+
+  function decodeB64(b64String: any) {
+    return new Uint8Array(Buffer.from(b64String, "base64"));
+  }
+
+  return { isInitialized, encryptWithTACo, decryptWithTACo };
 }
