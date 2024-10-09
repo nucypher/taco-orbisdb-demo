25ea75b2be28b1fb4e0bd881b16ac923a6c5d379
Manuel Montenegro <manuel@nucypher.com>
2024-10-09 18:26:28 +0200

    Adapt encryption/decryption calls to new TACo hook

--- app/(home)/(posts)/post/[slug]/page.tsx
+++ app/(home)/(posts)/post/[slug]/page.tsx
@@ -11,12 +11,13 @@ import "@/styles/mdx.css";
 
 import { useEffect, useState } from "react";
 import Link from "next/link";
+import { ethers } from "ethers";
 
 import { env } from "@/env.mjs";
 import { formatDate } from "@/lib/utils";
 import MaxWidthWrapper from "@/components/shared/max-width-wrapper";
 import { useODB } from "@/app/context/OrbisContext";
-import { decryptWithTACo } from "@/app/taco";
+import useTaco from "@/app/hooks/useTaco";
 
 const CONTEXT_ID = env.NEXT_PUBLIC_CONTEXT_ID ?? "";
 const COMMENT_ID = env.NEXT_PUBLIC_COMMENT_ID ?? "";
@@ -37,6 +38,8 @@ export default function PostPage({
   const [poststream, setPostStream] = useState<string | undefined>(undefined);
   const [comment, setComment] = useState<string | undefined>(undefined);
   const [commentFile, setCommentFile] = useState<File | undefined>(undefined);
+
+  const { isInitialized, decryptWithTACo } = useTaco();
 
   const uploadToIpfs = async () => {
     const uploadUrl = await upload({
@@ -74,7 +77,6 @@ export default function PostPage({
         console.log(updatequery);
 
         if (updatequery.content) {
-          alert("Created Comment.");
           await getPost(poststream!);
         }
       }
@@ -87,11 +89,17 @@ export default function PostPage({
   };
 
   const getPost = async (stream_id: string): Promise<void> => {
+    if (!window.ethereum) {
+      console.error("No Ethereum provider found");
+      return;
+    }
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+
     try {
       setPostStream(stream_id);
       const user = await orbis.getConnectedUser();
       if (user) {
-        console.log(stream_id);
         const query = await orbis
           .select()
           .raw(
@@ -117,12 +125,17 @@ export default function PostPage({
             `,
           )
           .run();
-        console.log(query);
+
         const postResult = query.rows as Post[];
         if (postResult.length) {
           setMessage(postResult[0]);
-          decryptWithTACo(postResult[0].body).then((decrypted) =>
-            setDecryptedBody(decrypted.toString()),
+          // Decrypt the post with TACo
+          decryptWithTACo(postResult[0].body, provider, signer).then(
+            (decrypted) => {
+              if (decrypted) {
+                setDecryptedBody(decrypted.toString());
+              }
+            },
           );
         }
       }
@@ -134,7 +147,7 @@ export default function PostPage({
 
   useEffect(() => {
     void getPost(params.slug);
-  }, [params.slug]);
+  }, [params.slug, isInitialized]);
 
   return (
     <>
@@ -175,7 +188,7 @@ export default function PostPage({
               </div>
             )}
             <p className="text-base text-muted-foreground md:text-lg">
-              {decryptedBody || message.body}
+              {decryptedBody || "<Hidden content>"}
             </p>
             <div className="mt-12 grid gap-5 bg-inherit lg:grid-cols-1">
               <div className="relative flex w-full items-center justify-center">
